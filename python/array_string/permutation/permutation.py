45b0f6a72243bdb477881db544ea314bca60c5ca
Olumide Ogundele <olumideralph@gmail.com>
2019-12-12 10:01:17 +0100

    implement permutation in python

---
+++ python/array_string/permutation/permutation.py
@@ -0,0 +1,38 @@
+class Solution:
+    def check_permutation(self, data_A: str, data_B: str) -> bool:
+        # validations
+        if len(data_A) != len(data_B) or not isinstance(data_A, str) \
+            or not isinstance(data_B, str):
+            return False
+
+        # key - index, value - character
+        data_A_info = {}
+
+        formed_data = list(data_B)
+
+        for i in range(len(data_A)):
+            if data_A_info.get(data_A[i]):
+                data_A_info[data_A[i]]["count"] += 1
+                data_A_info[data_A[i]]["index"].append(i)
+            else:
+                data_A_info[data_A[i]] = {"count": 1, "index": [i]}
+
+        # arrange data_B like data_A
+        for char in data_B:
+            if data_A_info.get(char):
+                index = data_A_info[char]["index"][0]
+
+                formed_data[index] = char
+
+                # we need to remove the used index incase of
+                # repeated characters
+                del data_A_info[char]["index"][0]
+                data_A_info[char]["count"] -= 1
+
+                if data_A_info[char]["count"] == 0:
+                    del data_A_info[char]
+
+        return "".join(formed_data) == data_A
+
+
+print(Solution().check_permutation("abgcdeg", "edcbagg"))
---
+++ python/array_string/permutation/permutation_test.py
@@ -0,0 +1,29 @@
+from unittest import TestCase
+
+from .permutation import Solution
+
+
+class TestCheckPermutation(TestCase):
+    def test_strings_same_character_different_order(self):
+        result = Solution().check_permutation("abcd", "dcba")
+        self.assertTrue(result)
+
+    def test_strings_different_character_different_order(self):
+        result = Solution().check_permutation("abcde", "dcba")
+        self.assertFalse(result)
+
+    def test_strings_with_different_length(self):
+        result = Solution().check_permutation("abcdhgg", "dcbag")
+        self.assertFalse(result)
+
+    def test_string_with_same_length_different_characters(self):
+        result = Solution().check_permutation("abcdef", "gedcba")
+        self.assertFalse(result)
+
+    def test_with_list_and_object(self):
+        result = Solution().check_permutation([], {})
+        self.assertFalse(result)
+
+    def test_with_empty_strings(self):
+        result = Solution().check_permutation("", "")
+        self.assertTrue(result)
