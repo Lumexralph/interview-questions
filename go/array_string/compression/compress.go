d72a95f0071db38539e10e38b8259e3261bd0d80
Olumide Ogundele <olumideralph@gmail.com>
2020-01-20 15:31:22 +0100

    implement feedback from PR

--- go/array_string/compression/compress.go
+++ go/array_string/compression/compress.go
@@ -11,26 +11,26 @@ func String(input string) string {
 	// use string builder to avoid memory allocation
 	// required to join new and old string
 	var output strings.Builder
-	charCount := make(map[string]int)
+	charCount := 0
 
 	for i := 0; i < len(input); i++ {
 		// collate the occurrence of the bytes
-		charCount[string(input[i])]++
+		charCount++
 		// look ahead index
 		if next := i + 1; next < len(input) {
 			if input[i] != input[next] {
 				// do a clean up and build the string
-				output.WriteString(string(input[i]))
-				count := strconv.Itoa(charCount[string(input[i])])
+				output.WriteByte(input[i])
+				count := strconv.Itoa(charCount)
 				output.WriteString(count)
 
 				// reset the charCount
-				delete(charCount, string(input[i]))
+				charCount = 0
 			}
 		} else {
 			// get the last byte result
-			output.WriteString(string(input[i]))
-			count := strconv.Itoa(charCount[string(input[i])])
+			output.WriteByte(input[i])
+			count := strconv.Itoa(charCount)
 			output.WriteString(count)
 		}
 	}
